086f71319ac5d554b5af757b87318bc0301661ed
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-12 11:55:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,14 +14,18 @@ def key_for_min_value(name_hash)
     if current_lowest_value == nil 
        current_lowest_value = v
        current_lowest_key = k 
-     elsif current_lowest_value > v 
+     else
+       if current_lowest_value > v 
          current_lowest_value = v 
          current_lowest_key = k 
+       end
      end
-  end
-  p  current_lowest_value 
+   end
+ p  current_lowest_value 
   p current_lowest_key 
-end
+ end
+      
+
 
 
 
