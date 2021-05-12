709bde148832a154cfdf71fb3b0cc225d9097ddb
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-12 11:59:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,14 +7,20 @@ name_hash = {:chair => 25, :table => 5, :mattress => 450}
 
 
 def key_for_min_value(name_hash)
+  
+#initilize two variables to hold the lowest_value and associated key and set to nil
   current_lowest_value = nil
   current_lowest_key = nil
   
+  #Iterate over the hash
   name_hash.each do |k,v|
+    
+  #if the current lowest variable is nil set the lowest value to value and set the lowest key to key
     if current_lowest_value == nil 
        current_lowest_value = v
        current_lowest_key = k 
      else
+   #if the current lowest variable is greater than value in the hash set the current lowest value to hash. Associate current_lowest_key to key with associated value
        if current_lowest_value > v 
          current_lowest_value = v 
          current_lowest_key = k 
